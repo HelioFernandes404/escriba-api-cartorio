cb0de87491106e208647d07b95a3ff3cf1cb1488
HelioFernandes404 <heliojobs25@gmail.com>
2024-03-23 09:45:37 -0300

    init do projeto

---
+++ src/main/java/org/helio/cartorioapi/CartorioapiApplication.java
@@ -0,0 +1,13 @@
+package org.helio.cartorioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CartorioapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CartorioapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/helio/cartorioapi/CartorioapiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.helio.cartorioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CartorioapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
